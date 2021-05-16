16d33e17c2d8da5f211e4cbe63629adcb511b86c
Jx3r11 <jx3r11@gmail.com>
2021-05-16 14:15:17 -0500

    Formulario

--- src/App_Form.js
+++ src/App_Form.js
@@ -13,7 +13,7 @@ function App() {
         
         <nav ClassName="Appmenu"> 
         <ul>
-          <li><a href="#">Inicio</a></li>
+          <li><a href="App.js">Inicio</a></li>
           <li><a href="#">Sobre nosotros</a></li>
           <li><a href="#">Contacto</a></li>
           <li><a href="#">Carrito</a></li>
@@ -24,14 +24,31 @@ function App() {
 
       <body className="Appbody">
 
-        <div ClassName="Appbaner">
-        <img src={baner} className="App-baner" alt="baner" />
+      <div className="App_Text_Form">¡Que gusto que estés aquí! dinos cómo podemos ayudarte en el siguiente formulario </div>
 
-        </div>
-
-        <div className="produto1"> 
-        <img src={productos} className="App-productos" alt="prodcutos" />
-        </div>
+<form ClassName="AppForm">
+<label>
+<div> Nombre:</div>
+<input type="text" name="name" />
+</label>
+<label>
+<div> Telefono:</div>
+<input type="text" name="phone" />
+</label>
+<label>
+<div> Correo:</div>
+<input type="text" name="mail" />
+</label>
+<label>
+<div> Asunto:</div>
+<input type="text" name="message" />
+</label>
+<label>
+<div> Si lo desea por favor dejenos el SKU que quiere comprar:</div>
+<input type="text" name="sku" />
+</label>
+<div><input type="submit" value="ENVIAR" /></div>
+</form>
 
       </body>
 
