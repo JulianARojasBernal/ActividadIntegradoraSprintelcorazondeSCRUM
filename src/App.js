2b28defd60da9399f6f368a12a9de4e5108f79b6
Julián A. Rojas <80870138+JulianARojasBernal@users.noreply.github.com>
2021-05-24 22:05:14 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -13,45 +13,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1> Las mejores herramientas...</h1>
-        
-<<<<<<< HEAD
-        <nav>
+        <nav ClassName="Appmenu">
         <img src={logo} width= '250' height='auto' ></img>
                     <a href='#'> Inicio </a>
                     <a href='#'> Sobre Nosotros </a>
                     <a href='#'> Tienda </a>
                     <a href='#'> Contacto </a>
-=======
-        <nav ClassName="Appmenu"> 
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Sobre nosotros</a></li>
-          <li><a href="App_Form.js">Contacto</a></li>
-          <li><a href="#">Carrito</a></li>
-        </ul>
->>>>>>> 3ae243e44905b0c3f4183f22b01641d4d1bd76c2
         </nav>
-
+        <h1> Las mejores herramientas...</h1>  
+      
       </header>
 
       <body className="Appbody">
-
-        
-
         <div ClassName="Appbaner">
-        <img src={baner} className="App-baner" alt="baner" />
-        <img src={baner1} className="App-baner" alt="baner" />
-        <img src={baner2} className="App-baner" alt="baner" />
-        <img src={baner3} className="App-baner" alt="baner" />
+          <img src={baner} className="App-baner" alt="baner" />
+          <img src={baner1} className="App-baner" alt="baner" />
+          <img src={baner2} className="App-baner" alt="baner" />
+          <img src={baner3} className="App-baner" alt="baner" />
         </div>
-      </body>
-
-<<<<<<< HEAD
-      <div ClassName="Containerproductos">
+      
+        <div ClassName="Containerproductos">
             <div ClassName="Productos">
               <img src={Productos} ClassName= "Producto" alt="producto" />
-              <h1 ClassName="tituloproducto"> Tornillo de banco 4" Foy </h1>
+              <h2 ClassName="tituloproducto"> Tornillo de banco 4" Foy </h2>
               <p className="info"> -Longitud total 235mm, apertura máxima de la quijada 90mm, profundidad de la garganta de 50mm, ancho de quijada de 100mm, ancho de quijada de 10KN. Diámetro de orificios de ensamble 9mm(+/- 5%). Peso 4.8 kg (+/-3%). 
               -Base giratoria de 360°.
               -Hecho en hierro fundido.
@@ -59,65 +43,37 @@ function App() {
               -Acabado: recubrimiento en polvo. Tipo de cara: áspera.
               -Dan el poder necesario para sujetar uno o más elementos de forma eficaz mientras son sometidos a diferentes operaciones mecánicas como aserrado, perforado, fresado, limado o marcado. </p>
             </div>
-            
-=======
-        <div className="App_Text_Form">¡Que gusto que estés aquí! dinos cómo podemos ayudarte en el siguiente formulario </div>
-
-        <form ClassName="AppForm">
-  <label>
-   <div> Nombre:</div>
-    <input type="text" name="name" />
-  </label>
-  <label>
-   <div> Telefono:</div>
-    <input type="text" name="phone" />
-  </label>
-  <label>
-  <div> Correo:</div>
-    <input type="text" name="mail" />
-  </label>
-  <label>
-  <div> Asunto:</div>
-    <input type="text" name="message" />
-  </label>
-  <label>
-  <div> Si lo desea por favor dejenos el SKU que quiere comprar:</div>
-    <input type="text" name="sku" />
-  </label>
-  <div><input type="submit" value="ENVIAR" /></div>
-</form>
-
-        <div className="produto1"> 
-        <img src={productos} className="App-productos" alt="prodcutos" />
         </div>
->>>>>>> 3ae243e44905b0c3f4183f22b01641d4d1bd76c2
-
-      </div>
-
+      
+      </body>
+      
       <footer>
-        <h2>Contactanos</h2>
-          <div className='container_footer'>
-            <div className='footer_section'>
+           
+        <div className='container_footer'>
+          <div className='footer_section'>
             <h3>Síguenos en nuestras redes sociales </h3>
-
-           <div className='footer_img'>
+          </div>
+          
+          <div className='footer_img'>
             <img src={face} width= '150' height='150' ></img>
             <img src={insta} width= '150' height='150' ></img>
             <img src={youtube} width= '150' height='150' ></img>
           </div>
-          </div>
+        
           
           <div className='footer_section'>
-              <h3>Telefonos</h3>
-              <p>333-555-4444</p>
-              <p>555-888-7946</p>
-            </div>
-            <div className='footer_section'>
-              <h3>Correos</h3>
-              <p>correo@correo.com</p>
-              <p>contacto@correo.com</p>
-            </div>
+            <h3>Telefonos</h3>
+            <p>333-555-4444</p>
+            <p>555-888-7946</p>
           </div>
+        
+          <div className='footer_section'>
+            <h3>Correos</h3>
+            <p>correo@correo.com</p>
+            <p>contacto@correo.com</p>
+          </div>
+        </div>
+        
       </footer>
     </div>
   );
